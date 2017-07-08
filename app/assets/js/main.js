a633909a03f69eab11caad7bea9d8695281cad12
Minhtri <minhtripr1605@gmail.com>
2017-07-08 14:23:35 +0700

    update Click Jquery

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -5,8 +5,20 @@ $(document).ready(function(){
             $window.on("scroll",function(){
                 var y=-($window.scrollTop()/($this.data('speed')/2));
                 var position="50% "+y+"px";
-                $this.css({backgroundPosition:position});
+                $this.css({"backgroundPosition":position});
             });
+        });
+        $(".one").on("click",function(){
+            $("#one").addClass('active').slideToggle();
+            $("#two,#three").removeClass('active').slideUp();
+        });
+        $(".two").on("click",function(){
+            $("#two").addClass('active').slideToggle();
+            $("#one,#three").removeClass('active').slideUp();
+        });
+        $(".three").on("click",function(){
+            $("#three").addClass('active').slideToggle();
+            $("#two,#one").removeClass('active').slideUp();
         });
 });
 
